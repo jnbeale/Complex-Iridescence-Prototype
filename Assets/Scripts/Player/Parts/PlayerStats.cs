ef0c0237c8ad02943724fd2da489d6a31d4660db
jassygirl00 <jasminebeale0521@gmail.com>
2022-12-09 21:43:20 -0600

    added damage flicker and damage cool down

--- Assets/Scripts/Player/Parts/PlayerStats.cs
+++ Assets/Scripts/Player/Parts/PlayerStats.cs
@@ -5,24 +5,56 @@ using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    
     public float health {get; private set;}
+
+    public SpriteRenderer playerSprite;
     private float maxHealth;
     public Slider healthSlider;
+
+    public bool canTakeDamage;
     private void Awake() 
     {
         health = 3f;
         maxHealth = health;
         healthSlider.value = 1;
+        canTakeDamage = true;
     }
 
 public void TakeDamage (int damage)
     {
+        
+        if(canTakeDamage == true){
         health -= damage;
+        StartCoroutine(Fade());
         healthSlider.value = health / maxHealth;
         if(health <= 0)
         {
             Destroy(this.gameObject);
             Application.Quit();
         }
+        }
     }
+
+    IEnumerator Fade()
+{
+    canTakeDamage = false;
+    for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
+    {
+        playerSprite.color = new Color(0f, 0f, 0f);
+        yield return new WaitForSeconds(.1f);
+        playerSprite.color = Color.white;
+        yield return new WaitForSeconds(.1f);
+        playerSprite.color = new Color(0f, 0f, 0f);
+        yield return new WaitForSeconds(.1f);
+        playerSprite.color = Color.white;
+        playerSprite.color = new Color(0f, 0f, 0f);
+        yield return new WaitForSeconds(.1f);
+        playerSprite.color = Color.white;
+        yield return new WaitForSeconds(3f);
+        canTakeDamage = true;
+    }
+}
+
+
 }
