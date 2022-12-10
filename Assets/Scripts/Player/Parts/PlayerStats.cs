651217e57c1f697aa5619f5c518aa3fa2661234d
erik-K <e.keckes8@gmail.com>
2022-12-10 10:17:14 +0100

    Revert "player health bar"
    This reverts commit bdf14b6643bbfa3257400a4b042a33fe845a6f09.

--- Assets/Scripts/Player/Parts/PlayerStats.cs
+++ Assets/Scripts/Player/Parts/PlayerStats.cs
@@ -1,24 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public float health {get; private set;}
-    private float maxHealth;
-    public Slider healthSlider;
     private void Awake() 
     {
-        health = 3f;
-        maxHealth = health;
-        healthSlider.value = 1;
+        health = 3f;    
     }
 
     public void TakeDamage (int damage)
     {
         health -= damage;
-        healthSlider.value = health / maxHealth;
+
         if(health <= 0)
         {
             Destroy(this.gameObject);
